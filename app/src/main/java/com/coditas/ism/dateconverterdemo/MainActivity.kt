4df4361b6aebf1c2fe88257110112d4fdbb5dae0
Sanskar-Pawar <sanskar.pawar@coditas.com>
2022-09-19 14:00:47 +0530

    changes in date sdk

--- app/src/main/java/com/coditas/ism/dateconverterdemo/MainActivity.kt
+++ app/src/main/java/com/coditas/ism/dateconverterdemo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.coditas.ism.dateconverterdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.coditas.ism.dateconverterdemo.databinding.ActivityMainBinding
+import com.coditas.ism.dateshowdemo.DateConverter
 
 class MainActivity : AppCompatActivity() {
     private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -14,9 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initView(){
         mBinding.run {
-            val mInputDate = etDate.text.toString()
             btnConvertDate.setOnClickListener{
-
+                val mInputDate = etDate.text.toString()
+                Log.d("edit text date","$mInputDate")
+                val outputDate = DateConverter.getDate(mInputDate.toInt())
+                tvConvertedDate.text =outputDate
             }
         }
 
