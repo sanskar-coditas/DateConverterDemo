48c7608232de33fcb619b84fc25ea324e59e2001
Sanskar-Pawar <sanskar.pawar@coditas.com>
2022-09-19 11:19:31 +0530

    Added Ui implimentation

--- app/src/main/java/com/coditas/ism/dateconverterdemo/MainActivity.kt
+++ app/src/main/java/com/coditas/ism/dateconverterdemo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.coditas.ism.dateconverterdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.coditas.ism.dateconverterdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+        initView()
+    }
+    private fun initView(){
+        mBinding.run {
+            val mInputDate = etDate.text.toString()
+            btnConvertDate.setOnClickListener{
+
+            }
+        }
+
+
     }
 }
